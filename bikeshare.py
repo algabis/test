29e21590af0b5c57a8802057f61c4e3405c4a4f2
algabis <algabis@hotmail.com>
2020-12-05 09:34:16 +0300

    deleted extra spaces to clean code

--- bikeshare.py
+++ bikeshare.py
@@ -203,7 +203,6 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
@@ -212,7 +211,6 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
 
 if __name__ == "__main__":
     main()
